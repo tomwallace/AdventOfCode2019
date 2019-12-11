ffe6be0063859cfb0d6e793043914f11e27dbdf8
Tom Wallace <thomaspwallace@gmail.com>
2019-12-11 06:52:03 -0500

    Adding Day 10 solutions and tests.

---
+++ AdventOfCode2019.Tests/DayTenTests.cs
@@ -0,0 +1,52 @@
+﻿using AdventOfCode2019.Ten;
+using Xunit;
+
+namespace AdventOfCode2019.Tests
+{
+    public class DayTenTests
+    {
+        [Theory]
+        [InlineData(@"Ten\DayTenTestInputAA.txt", 8)]
+        [InlineData(@"Ten\DayTenTestInputA.txt", 33)]
+        [InlineData(@"Ten\DayTenTestInputB.txt", 35)]
+        [InlineData(@"Ten\DayTenTestInputC.txt", 41)]
+        [InlineData(@"Ten\DayTenTestInputD.txt", 210)]
+        public void HowManyAsteroidsSeenFromBestLocation(string filePath, int expected)
+        {
+            var sut = new DayTen();
+            var result = sut.HowManyAsteroidsSeenFromBestLocation(filePath);
+            var numberSeen = result.Distances.Count;
+
+            Assert.Equal(expected, numberSeen);
+        }
+
+        [Fact]
+        public void RunAsteroidRoutine()
+        {
+            string filePath = @"Ten\DayTenTestInputE.txt";
+            var sut = new DayTen();
+            var result = sut.RunAsteroidRoutine(filePath, 9);
+
+            Assert.Equal(901, result);
+        }
+
+        [Fact]
+        public void PartA_Actual()
+        {
+            var sut = new DayTen();
+            var result = sut.PartA();
+
+            Assert.Equal("263", result);
+        }
+
+        // 2926 is too high
+        [Fact]
+        public void PartB_Actual()
+        {
+            var sut = new DayTen();
+            var result = sut.PartB();
+
+            Assert.Equal("1110", result);
+        }
+    }
+}
--- AdventOfCode2019/Ten/DayTen.cs
+++ AdventOfCode2019/Ten/DayTen.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
-using AdventOfCode2019.Utility;
+﻿using AdventOfCode2019.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode2019.Ten
 {
@@ -17,48 +19,147 @@ namespace AdventOfCode2019.Ten
 
         public string PartA()
         {
-            //IntCodeComputer.IntCodeComputer computer = new IntCodeComputer.IntCodeComputer(INPUT, 1);
-            //computer.ProcessInstructions();
-            return ""; //computer.GetDiagnosticCode().ToString();
+            string filePath = @"Ten\DayTenInput.txt";
+            int calculation = HowManyAsteroidsSeenFromBestLocation(filePath).Distances.Count;
+
+            return calculation.ToString();
         }
 
         public string PartB()
         {
-            //IntCodeComputer.IntCodeComputer computer = new IntCodeComputer.IntCodeComputer(INPUT, 5);
-            //computer.ProcessInstructions();
-            return ""; //computer.GetDiagnosticCode().ToString();
-        }
-        /*
-        public int HowManyAsteroidsSeenFromBestLocation(string filePath, int rows, int cols)
-        {
-            char[,] asteroidBelt = MakeAsteroidBelt(filePath, rows, cols);
+            string filePath = @"Ten\DayTenInput.txt";
+            int destroyed = RunAsteroidRoutine(filePath, 200);
 
+            return destroyed.ToString();
         }
 
-        public int? CalculateAsteroidsSeen(char[,] asteroidBelt, int x, int y)
+        public BestLocationOutput HowManyAsteroidsSeenFromBestLocation(string filePath)
         {
-            // If not an asteroid, return null
-            if (asteroidBelt[x, y] == '.')
-                return null;
+            List<Asteroid> asteroidBelt = MakeAsteroidBelt(filePath);
+            BestLocationOutput bestLocationOutput = new BestLocationOutput();
+            int bestCount = 0;
 
+            for (int a = 0; a < asteroidBelt.Count; a++)
+            {
+                Asteroid currentAsteroid = asteroidBelt[a];
+                Dictionary<double, List<AsteroidDistance>> distances = new Dictionary<double, List<AsteroidDistance>>();
+                for (int c = 0; c < asteroidBelt.Count; c++)
+                {
+                    if (a != c)
+                    {
+                        Asteroid comparing = asteroidBelt[c];
+                        double angle = currentAsteroid.GetComparisonAngle(comparing);
+                        int distance = currentAsteroid.GetDistance(comparing);
+                        AsteroidDistance asteroidDistance = new AsteroidDistance() { Asteroid = comparing, Distance = distance };
+                        if (distances.ContainsKey(angle))
+                            distances[angle].Add(asteroidDistance);
+                        else
+                            distances.Add(angle, new List<AsteroidDistance>() { asteroidDistance });
+                    }
+                }
 
+                // Sort distances
+                double[] keys = distances.Keys.ToArray();
+                for (int i = 0; i < distances.Count; i++)
+                {
+                    List<AsteroidDistance> distanceSet = distances[keys[i]];
+                    List<AsteroidDistance> sorted = distanceSet.OrderByDescending(d => d.Distance).ToList();
+                    distances[keys[i]] = sorted;
+                }
+
+                if (distances.Count > bestCount)
+                {
+                    bestCount = distances.Count;
+                    bestLocationOutput.BestLocation = currentAsteroid;
+                    bestLocationOutput.Distances = distances;
+                }
+            }
+
+            return bestLocationOutput;
         }
-        */
-        private char[,] MakeAsteroidBelt(string filePath, int rows, int cols)
+
+        public int RunAsteroidRoutine(string filePath, int numberAsteroidsToDestroy)
         {
-            char[,] asteroidBelt = new char[cols,rows];
+            BestLocationOutput output = HowManyAsteroidsSeenFromBestLocation(filePath);
+            List<Asteroid> destroyed = new List<Asteroid>();
+            int angleCounter = 0;
+            double[] sortedAngles = output.Distances.Keys.OrderByDescending(d => d).ToArray();
+            do
+            {
+                double currentAngle = sortedAngles[angleCounter];
+                // vaporize!
+                destroyed.Add(output.Distances[currentAngle].First().Asteroid);
+                output.Distances[currentAngle].RemoveAt(0);
+
+                angleCounter++;
+            } while (destroyed.Count < numberAsteroidsToDestroy);
+
+            return destroyed.Last().X * 100 + destroyed.Last().Y;
+        }
+
+        private List<Asteroid> MakeAsteroidBelt(string filePath)
+        {
+            List<Asteroid> asteroidBelt = new List<Asteroid>();
 
             List<string> fileLines = FileUtility.ParseFileToList(filePath, line => line);
-            for (int y = 0; y < rows; y++)
+            for (int y = 0; y < fileLines.Count; y++)
             {
                 char[] row = fileLines[y].ToCharArray();
-                for (int x = 0; y < cols; x++)
+                for (int x = 0; x < row.Length; x++)
                 {
-                    asteroidBelt[x, y] = row[x];
+                    if (row[x] == '#')
+                        asteroidBelt.Add(new Asteroid(x, y));
                 }
             }
 
             return asteroidBelt;
         }
+    }
+
+    public class Asteroid
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Asteroid(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double GetComparisonAngle(Asteroid comparing)
+        {
+            // Geometry is not my thing, so I had to look this angle calculation up
+            // https://en.wikipedia.org/wiki/Atan2
+            int diffX = comparing.X - X;
+            int diffY = comparing.Y - Y;
+
+            return Math.PI / 2 + Math.Atan2(diffX, diffY);
+        }
+
+        public int GetDistance(Asteroid comparing)
+        {
+            int xDistance = Math.Abs(comparing.X - X);
+            int yDistance = Math.Abs(comparing.Y - Y);
+
+            return xDistance + yDistance;
+        }
+
+        public override string ToString()
+        {
+            return $"[{X},{Y}]";
+        }
+    }
+
+    public class AsteroidDistance
+    {
+        public Asteroid Asteroid { get; set; }
+        public int Distance { get; set; }
+    }
+
+    public class BestLocationOutput
+    {
+        public Asteroid BestLocation { get; set; }
+        public Dictionary<double, List<AsteroidDistance>> Distances { get; set; }
     }
 }
